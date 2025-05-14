a3162b85ea07aecd927a510aa7f332a4a55d1c24
Paulie-Aditya <paul.aditya304@gmail.com>
2025-05-15 04:23:48 +0530

    more links fixed

--- assets/js/search.js
+++ assets/js/search.js
@@ -103,7 +103,7 @@ function coinsResult(coins) {
         `;
         table.appendChild(row);
         row.onclick = () => {
-            window.location.href = `../../pages/coin.html?coin=${coin.id}`;
+            window.location.href = `../../crypto-marketplace/pages/coin.html?coin=${coin.id}`;
         };
     });
     coinsList.appendChild(table);
